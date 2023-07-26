7b1dd670acf788a2ad15827c586237c425f65313
Jacob <weaver.jacob.t@gmail.com>
2023-07-25 19:44:49 -0500

    took a bit to find time

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+
+function writeCards (messages, event) {
+    const yourMessage = [];
+    for (let i = 0; i < messages.length; i++)
+    {    
+        const thankYou = "Thank you, " + messages[i] + ", for the wonderful " + event + " gift!";
+        
+        yourMessage.push(thankYou);
+       
+        
+    }
+     return yourMessage;
+
+} 
+
+function countDown (startNum) {
+
+while (startNum > -1) {
+
+  console.log(startNum--);
+
+}
+
+
+
+}
